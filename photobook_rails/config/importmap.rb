775fd17859580beb384de05d49993cf9b14a3651
divo <divoapple@gmail.com>
2022-10-28 21:54:12 +0100

    Hack canvas-sketch built output into javascript assets
    It's awful but it does work. I can probably clean it up

--- photobook_rails/config/importmap.rb
+++ photobook_rails/config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "canvas-sketch", to: "https://ga.jspm.io/npm:canvas-sketch@0.7.6/dist/canvas-sketch.umd.js"
-pin "p5", to: "https://ga.jspm.io/npm:p5@1.5.0/lib/p5.min.js"
+pin_all_from "app/javascript/custom", under: "custom"
+# pin "p5", to: "https://ga.jspm.io/npm:p5@1.5.0/lib/p5.min.js"
+pin "p5", to: "https://ga.jspm.io/npm:p5@1.5.0/lib/p5.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
