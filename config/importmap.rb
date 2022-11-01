75c5385a4f50a1dff6c378ffaec87d8f40fb38ef
divo <divoapple@gmail.com>
2022-11-01 16:30:16 +0000

    To hell with packaging JS, just dump in a script tag into the main page
    I got packaging working somewhat with importmap and esbuild (in another project) after a lot of wasted time
    but for whatever reason it's not loaded in time for the page to use it? Timebox over, fuck this
    dump a script tag into the page. I only need one package anyway

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/custom", under: "custom"
-# pin "p5", to: "https://ga.jspm.io/npm:p5@1.5.0/lib/p5.min.js"
-pin "p5", to: "https://ga.jspm.io/npm:p5@1.5.0/lib/p5.js"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
